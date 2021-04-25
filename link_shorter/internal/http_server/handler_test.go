4f32f6d4c40b0192bcd6c8b396be7560261ace86
matam <matamyn7@gmail.com>
2021-04-26 02:32:12 +0300

    Добавлен http_server и обработчики для соответствующих REST запросов

--- link_shorter/internal/http_server/handler_test.go
+++ link_shorter/internal/http_server/handler_test.go
@@ -1,17 +1,19 @@
 package http_server
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/matamyn/tech_assignment_GO/link_shorter/internal/common"
 	"github.com/matamyn/tech_assignment_GO/link_shorter/internal/db_facade"
+	"github.com/matamyn/tech_assignment_GO/link_shorter/internal/model"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-func TestHttpServer_GetLink(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/HELLO", nil)
+func TestHttpServer(t *testing.T) {
 
 	//todo: Добавить тестовый контур для фасада.
 	facade, err := db_facade.InitDbFacade(&common.NewConfig().DataBase)
@@ -20,6 +22,24 @@ func TestHttpServer_GetLink(t *testing.T) {
 	}
 	defer facade.Db_.Close()
 	s := newHttpServer(facade)
-	s.ServeHttp(rec, req)
-	assert.Equal(t, rec.Code, http.StatusOK)
+
+	full_link := "{\"Link\": \"Test_Fake_link_" + time.Now().String() + "\""
+
+	b := &bytes.Buffer{}
+	json.NewEncoder(b).Encode(struct {
+		Url string
+	}{Url: full_link})
+	set_rec := httptest.NewRecorder()
+	set_req, _ := http.NewRequest(http.MethodPost, "/SetLink", b)
+	s.router.ServeHTTP(set_rec, set_req)
+
+	get_rec := httptest.NewRecorder()
+	json.NewEncoder(b).Encode(struct {
+		ShortUrl string
+	}{ShortUrl: model.DefaultLink + set_rec.Body.String()})
+	get_req, _ := http.NewRequest(http.MethodPost, "/GetLink", b)
+	s.router.ServeHTTP(get_rec, get_req)
+
+	assert.Equal(t, get_rec.Body.String(), full_link)
+
 }
